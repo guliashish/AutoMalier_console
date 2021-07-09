564bdd1f3a4f342186095901d035b7c838f3de18
guliashish <60592507+guliashish@users.noreply.github.com>
2021-07-09 18:23:13 +0530

    Add files via upload

---
+++ AutoMail1/AutoMail1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+
+namespace AutoMail1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+
+           string[] ServerList = new string[] { "www.google.com", "www.facebook.com" };
+
+
+            Console.WriteLine( ServerStatus(ServerList));
+        }
+
+        static string ServerStatus(string[] serverList)
+        {
+            Ping pingSender = new Ping();
+            PingReply reply = pingSender.Send(serverList[0]);
+            Console.WriteLine("Status of Host: {0}", serverList[0]);
+            if (reply.Status == IPStatus.Success)
+            {
+                 
+                string ServerStatus = " "+ reply.Address.ToString() + " , " + reply.Status.ToString() + "  ";
+                return ServerStatus;
+            }
+            else
+            {
+                string ServerStatus = " " + reply.Address.ToString() + " , " + reply.Status.ToString() + "  ";
+                return ServerStatus;
+            }
+            
+        }
+
+    }
+
+}
+
